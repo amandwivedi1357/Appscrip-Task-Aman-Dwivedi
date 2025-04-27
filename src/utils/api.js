bc3b08abfe5388daa5e75c85e5f9f6000bce4c76
Aman Dwivedi <amandwivedi71124.com>
2025-04-27 23:49:03 +0530

    added appscrip assignment

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+
+
+export const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return [];
+  }
+};
